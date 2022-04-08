3ba1a6126664d79e3e1f05d3e2383677e8cc3076
DiegoRafael <drafael.flomar@gmail.com>
2022-04-07 23:01:16 -0500

    first commit eureka server

---
+++ src/test/java/com/drafael/springboot/professional/eureka/SpringBootEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drafael.springboot.professional.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
